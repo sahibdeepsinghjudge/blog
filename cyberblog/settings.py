f47477d21bda522b8e6398774d8337e5e422b416
Sahibdeep Singh <64653787+sahibdeepsinghjudge@users.noreply.github.com>
2022-02-11 14:05:38 +0530

    Update settings.py

--- cyberblog/settings.py
+++ cyberblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$n$=2dp(@#o#bkbw83x_nvkvtlb8u%93bp74f3nl4udn@*=m@$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
